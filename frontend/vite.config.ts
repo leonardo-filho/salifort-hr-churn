fed2e9406f57fea8e641097ccbcb729891a94f4c
leo-filho <leronasfilho@gmail.com>
2025-09-22 14:25:07 -0300

    bug fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/salifort-hr-churn/',
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-router-dom', 'recharts', 'react-dom'],
-          // Outras bibliotecas que podem ser grandes
         },
       },
     },
